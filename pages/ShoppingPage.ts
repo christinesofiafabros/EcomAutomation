f18039829da7f0b390eb2430758eeb6e77977ae7
pi <sbcuat@SBCs-MacBook-Pro.local>
2026-09-08 13:29:07 +0800

    Added sort tests

--- pages/ShoppingPage.ts
+++ pages/ShoppingPage.ts
@@ -16,6 +16,18 @@ export class ShoppingPage {
         await this.page.getByTestId('sort-select').selectOption('price-asc')
     }
 
+    async sortProductsByNameAsc() {
+        await this.page.getByTestId('sort-select').selectOption('name-asc')
+    }
+
+    async sortProductsByPriceDesc() {
+        await this.page.getByTestId('sort-select').selectOption('price-desc')
+    }
+
+    async sortProductsByNameDesc() {
+        await this.page.getByTestId('sort-select').selectOption('name-desc')
+    }
+
     async getProductsByPrice() {
         const productPrices = this.page.getByTestId(/product-price-/);
         const priceTexts = productPrices.allTextContents();
@@ -27,8 +39,4 @@ export class ShoppingPage {
         const nameTexts = productNames.allTextContents();
         return nameTexts;
     }   
-
-    async sortProductsByNameAsc() {
-        await this.page.getByTestId('sort-select').selectOption('name-asc')
-    }
 }
